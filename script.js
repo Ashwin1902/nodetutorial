7e1f8c1810f1256cc37a43df07a466f588a0414f
ashwi <ashwinbansal19feb@gmail.com>
2023-11-25 19:54:38 +0530

    Mongo

---
+++ script.js
@@ -0,0 +1,16 @@
+// use of ejs 
+
+
+const express = require('express');
+const app=express();
+
+app.set("view engine","ejs");
+app.use(express.static("./public"));
+
+app.get('/',(req,res)=>{
+    res.render("index");
+})
+
+app.listen(5000,()=>{
+    console.log("setup done");
+})
